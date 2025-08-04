aaaaaf3840248718ea6a3d958cb595eec5902369
Kishori-Tipugade <tipugadekishori@gmail.com>
2025-08-04 14:54:14 +0530

    first commit

---
+++ client/src/api/axiosInstance.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
+    withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = sessionStorage.getItem("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+export default axiosInstance;
